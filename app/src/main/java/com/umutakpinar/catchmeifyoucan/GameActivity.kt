cf683bf5cdce67c21f81769f4fc60818bbe3b7aa
Umut Akpinar <umtakpnr5@gmail.com>
2022-10-06 00:32:00 +0300

    Needs improvement but stable to play

--- app/src/main/java/com/umutakpinar/catchmeifyoucan/GameActivity.kt
+++ app/src/main/java/com/umutakpinar/catchmeifyoucan/GameActivity.kt
@@ -10,9 +10,11 @@ import android.os.CountDownTimer
 import android.os.Handler
 import android.os.Looper
 import android.view.View
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import com.umutakpinar.catchmeifyoucan.databinding.ActivityGameBinding
+import java.util.Random
 
 class GameActivity : AppCompatActivity() {
     private lateinit var binding : ActivityGameBinding
@@ -20,10 +22,16 @@ class GameActivity : AppCompatActivity() {
     private var godMode = false
     private lateinit var sharedPreferences: SharedPreferences
     private var bestScore : Int = 0
-    //Runnable
+    //Runnables
+    //for timer
     private var runnable : Runnable = Runnable{ }
     private var handler : Handler = Handler(Looper.getMainLooper())
     private var time : Int = 16
+    //for random visibilty changer
+    var randomRunnable : Runnable = Runnable {}
+    var randomHandler : Handler = Handler(Looper.getMainLooper())
+    //Image Array
+    var imageArray = ArrayList<View>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityGameBinding.inflate(layoutInflater)
@@ -43,11 +51,14 @@ class GameActivity : AppCompatActivity() {
             Toast.makeText(this@GameActivity,"God mode enabled!",Toast.LENGTH_SHORT).show()
         }
 
-        //setDisable go text
-        binding.textViewInfo.isEnabled = false
+        //set invisinle go text
+        binding.textViewInfo.visibility = View.INVISIBLE
 
         //startCountDownfor bgin
         startCountDownForBeginingAndStartTheGame()
+
+        // imageArray'e görselleri ekle ve tamamını öncelikle invisible yap
+        hideImagesAndSetAllArray()
     }
 
     fun caught(view: View){
@@ -86,6 +97,7 @@ class GameActivity : AppCompatActivity() {
                 if(time > 0){
                     time--
                     showTimeText()
+                    print("Time : $time")
                     handler.postDelayed(this,1000)
                 }else {
                     stopTimer() //goyazısını sil ve kırmızı yap score'u kaydet ve ekranda göster!
@@ -120,20 +132,54 @@ class GameActivity : AppCompatActivity() {
 
             override fun onFinish() {
                 startTimerAndGame()
-                binding.textViewInfo.isEnabled = true
+                showRandomKenny()
+                binding.textViewInfo.visibility = View.VISIBLE
             }
 
-        }
+        }.start()
     }
 
     private fun stopTimer(){
         handler.removeCallbacks(runnable)
+        randomHandler.removeCallbacks(randomRunnable)
         time = 16
-        showTimeText()
     }
 
     private fun showTimeText(){
         val timeText = "Time: $time"
         binding.textViewTime.text = timeText
+    }
+
+    private fun showRandomKenny(){
+
+        var delayMillis : Long = 500
+        if(godMode){
+            delayMillis = 250
+        }
+
+        randomRunnable = Runnable {
+            val rnd = Random()
+            val index = rnd.nextInt(imageArray.size)
+            hideAllImages()
+            imageArray[index].visibility = View.VISIBLE
+            randomHandler.postDelayed(randomRunnable,delayMillis)
+        }
+
+        randomHandler.post(randomRunnable)
+
+    }
+
+    private fun hideImagesAndSetAllArray(){
+        for(item : Int in 0..15){
+            val element = binding.gridLayout3.getChildAt(item)
+            imageArray.add(element)
+            element.visibility = View.INVISIBLE
+        }
+    }
+
+    private fun hideAllImages(){
+        for(item : Int in 0..15){
+            binding.gridLayout3.getChildAt(item).visibility = View.INVISIBLE
+        }
     }
 }
